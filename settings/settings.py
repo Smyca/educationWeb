d4cd153f6867aa78815b2c3eab696639ce69b7c9
Smyca <samuelarcon40@gmail.com>
2025-04-03 12:51:21 -0300

    change: settings

--- settings/settings.py
+++ settings/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=yk8lqc(c)^xc1gh9rut!qj=!^m7kgw%706$dx&04%d=14^t%=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
