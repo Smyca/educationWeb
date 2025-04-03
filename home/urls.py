b5c5e2363de02b0dabf685b5e19fba4b7882b75a
Smyca <samuelarcon40@gmail.com>
2025-04-03 12:23:14 -0300

    commit 2

---
+++ home/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import homeView
+
+
+
+
+urlpatterns = [
+    path('', homeView),
+
+]
--- settings/settings.py
+++ settings/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # "livereload",
+    'home.apps.HomeConfig'
 ]
 
 MIDDLEWARE = [
